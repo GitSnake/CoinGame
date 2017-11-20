81a3982915abd8551cc9ba17c7f908cbc2ac056c
CodeVyper <aaron.kiewe@gmail.com>
2017-11-20 01:20:39 -0500

    Added Switch case

--- js/counter.js
+++ js/counter.js
@@ -1,3 +1,5 @@
+// Counter function
+
 function modify_qty(val) {
     var qty = document.getElementById('qty').value;
     var new_qty = parseInt(qty,10) + val;
@@ -9,3 +11,32 @@ function modify_qty(val) {
     document.getElementById('qty').value = new_qty;
     return new_qty;
 }
+
+// switch case
+
+oCount.addEventListener('click', function(e) {
+    e.preventDefault();
+    nCount += 1;
+    switch(nCount) {
+        case 1:
+            oResult.innerHTML = 'penny';
+            break;
+        case 2:
+            oResult.innerHTML = 'nickle';
+            break;
+        case 3:
+            oResult.innerHTML = 'dime';
+            break;
+        case 4:
+            oResult.innerHTML = 'quarter';
+            break;
+        case 5:
+            oResult.innerHTML = 'loonie';
+            break;
+        case 6:
+            oResult.innerHTML = 'twonnie';
+            break;
+        default:
+            oResult.innerHTML = 'No change';
+    }
+}, false); 
