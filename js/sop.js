9d24e54fea874047c8eaf9df5dc5378e609a9e22
CodeVyper <aaron.kiewe@gmail.com>
2017-11-20 01:32:25 -0500

    SOP - Outline of Project

---
+++ js/sop.js
@@ -0,0 +1,8 @@
+// create coin object
+// fill object with image value
+// fill object with number value
+// create interface
+// create random number generator
+// create counter
+// match random number to counter
+// display winner screen and next level
